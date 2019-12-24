2fb805f6c06b319d99b64da20b55ebc6b03edbd2
Soumya Dey <soumyadey200@hotmail.com>
2019-12-24 14:10:24 +0530

    added preloader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").classList.add("loading-finish");
+});
+
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".link-list");
