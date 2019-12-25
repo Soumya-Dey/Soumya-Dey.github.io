8c998306da68acf898029b832bf2628e9a3f7fbe
Soumya Dey <soumyadey200@hotmail.com>
2019-12-25 23:49:50 +0530

    fixed nav not opening

--- js/script.js
+++ js/script.js
@@ -2,13 +2,25 @@ window.addEventListener("load", () => {
   document.querySelector(".preloader").classList.add("loading-finish");
 });
 
-const hamburger = document.querySelector(".hamburger");
+var hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".link-list");
 const line1 = document.getElementById("line-1");
 const line2 = document.getElementById("line-2");
 const line3 = document.getElementById("line-3");
 const bars = document.querySelectorAll(".bar");
+
+hamburger.addEventListener("click", function() {
+  navLinks.classList.toggle("open-nav");
+
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+
+  line1.classList.toggle("hover");
+  line2.classList.toggle("hover");
+  line3.classList.toggle("hover");
+});
 
 bars.forEach(bar => {
   bar.style.display = "none";
@@ -41,15 +53,3 @@ function scrollFunction(topPx) {
     });
   }
 }
-
-hamburger.addEventListener("click", function() {
-  navLinks.classList.toggle("open-nav");
-
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
-
-  line1.classList.toggle("hover");
-  line2.classList.toggle("hover");
-  line3.classList.toggle("hover");
-});
