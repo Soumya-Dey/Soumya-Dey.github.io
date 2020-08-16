eb7240c818fa8288a98c0dba36ee2cc8a260e763
Soumya Dey <soumyadey200@hotmail.com>
2020-08-16 21:58:45 +0530

    rewrote portfolio in react

---
+++ precache-manifest.d8ac1e498c1a8e07a95f8d5c4e41f6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233aad8d0706ec5ef6e3d60cd4362011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "844a9b9e84d696b310ef",
+    "url": "/static/css/main.358feaca.chunk.css"
+  },
+  {
+    "revision": "99a58e4e2ae084740dee",
+    "url": "/static/js/2.84bff0a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84bff0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844a9b9e84d696b310ef",
+    "url": "/static/js/main.0ddd6511.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
